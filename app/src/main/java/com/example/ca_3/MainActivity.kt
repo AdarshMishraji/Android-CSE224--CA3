11e9e0e5278acac709daa71cf8cfbfd9ed0957d7
Adarsh Mishra <adarshmishra969@gmail.com>
2021-12-07 10:28:09 +0530

    little bit changes

--- app/src/main/java/com/example/ca_3/MainActivity.kt
+++ app/src/main/java/com/example/ca_3/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.ca_3
 
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -12,47 +10,68 @@ import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
-        val name= findViewById<EditText>(R.id.name);
-        val email= findViewById<EditText>(R.id.email);
-        val spinner: Spinner = findViewById(R.id.qual_spinner);
-        val submitBtn = findViewById<Button>(R.id.button);
-        val male_radio = findViewById<RadioButton>(R.id.male_btn);
-        val female_radio = findViewById<RadioButton>(R.id.female_btn);
-        val tnc = findViewById<CheckBox>(R.id.tnc);
+        val name= findViewById<EditText>(R.id.name)
+        val email= findViewById<EditText>(R.id.email)
+        val spinner: Spinner = findViewById(R.id.qual_spinner)
+        val submitBtn = findViewById<Button>(R.id.button)
+        val maleRadio = findViewById<RadioButton>(R.id.male_btn)
+        val femaleRadio = findViewById<RadioButton>(R.id.female_btn)
+        val tnc = findViewById<CheckBox>(R.id.tnc)
 
         ArrayAdapter.createFromResource(
             this,
             R.array.qual_items,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.adapter = adapter;
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
 
-        submitBtn.setOnClickListener {
-            if (name.text.isNullOrBlank()) return@setOnClickListener showSnack(it, "Empty Name field.");
-            if (email.text.isNullOrBlank() or !isValidEmail(email.text.toString())) return@setOnClickListener showSnack(it, "Invalid Email");
-            if (!tnc.isChecked) return@setOnClickListener showSnack(it, "TNC must be checked");
+        name.setOnFocusChangeListener { _, hasFocus -> if (hasFocus) {
+                    name.setBackgroundResource(R.drawable.border)
+                } else {
+                    name.setBackgroundResource(R.color.background)
+                }
+            }
 
-            intent = Intent(this, Result::class.java);
-            intent.putExtra("name", name.text.toString());
-            intent.putExtra("email", email.text.toString());
-            intent.putExtra("gender", if (male_radio.isChecked) "Male" else if (female_radio.isChecked) "Female" else null);
-            intent.putExtra("highest_qual", spinner.selectedItem.toString());
-            startActivity(intent);
+        email.setOnFocusChangeListener { _, hasFocus -> if (hasFocus) {
+                    email.setBackgroundResource(R.drawable.border)
+                } else {
+                    email.setBackgroundResource(R.color.background)
+                }
+            }
+
+        submitBtn.setOnClickListener {
+            if (name.text.isNullOrBlank()) {
+                name.setBackgroundResource(R.drawable.border_error)
+                return@setOnClickListener showSnack(it, "Empty Name field.")
+            }
+
+            if (email.text.isNullOrBlank() or !isValidEmail(email.text.toString())){
+                email.setBackgroundResource(R.drawable.border_error)
+                return@setOnClickListener showSnack(it, "Invalid Email")
+            }
+
+            if (!tnc.isChecked) return@setOnClickListener showSnack(it, "TNC must be checked")
+
+            intent = Intent(this, Result::class.java)
+            intent.putExtra("name", name.text.toString())
+            intent.putExtra("email", email.text.toString())
+            intent.putExtra("gender", if (maleRadio.isChecked) "Male" else if (femaleRadio.isChecked) "Female" else null)
+            intent.putExtra("highest_qual", spinner.selectedItem.toString())
+            startActivity(intent)
         }
 
     }
 
     private fun showSnack(context:View, text:String){
-        Snackbar.make(context, text, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(context, text, Snackbar.LENGTH_LONG).show()
     }
 
     private fun isValidEmail (email:String):Boolean{
@@ -60,8 +79,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_menu, menu);
-        return true;
+        menuInflater.inflate(R.menu.action_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -70,31 +89,24 @@ class MainActivity : AppCompatActivity() {
                 val dialog = AlertDialog.Builder(this)
                 dialog.setTitle(R.string.about_me)
 
-                var image = ImageView(this);
-                image.setImageResource(R.drawable.dp);
+                val image = ImageView(this)
+                image.setImageResource(R.drawable.dp)
                 image.layoutParams = ConstraintLayout.LayoutParams(
                     ConstraintLayout.LayoutParams.WRAP_CONTENT,
                     ConstraintLayout.LayoutParams.WRAP_CONTENT
-                );
-                image.setPadding(0,50,0,50);
+                )
+                image.setPadding(0,50,0,50)
 
-                dialog.setCustomTitle(image);
-                dialog.setMessage(R.string.my_info);
-                dialog.setPositiveButton("Close") { dialogInterface, _ ->
-                    {
-                        dialogInterface.dismiss();
-                    }
-                }
-                dialog.show();
-
-                true;
+                dialog.setCustomTitle(image)
+                dialog.setMessage(R.string.my_info)
+                dialog.setPositiveButton("Close") { dialogInterface, _ -> dialogInterface.dismiss() }
+                dialog.show()
             }
             R.id.exit_app -> {
-                finish();
-                true;
+                finish()
             }
-            else -> super.onOptionsItemSelected(item);
+            else -> super.onOptionsItemSelected(item)
         }
-        return true;
+        return true
     }
 }
--- app/src/main/java/com/example/ca_3/Result.kt
+++ app/src/main/java/com/example/ca_3/Result.kt
@@ -1,29 +1,35 @@
 package com.example.ca_3
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Result : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.result);
+        setContentView(R.layout.result)
 
-        val res_name = findViewById<TextView>(R.id.res_name);
-        val res_email = findViewById<TextView>(R.id.res_email);
-        val res_gender = findViewById<TextView>(R.id.res_gender);
-        val res_highest_qual = findViewById<TextView>(R.id.res_highest_qual);
+        val resName = findViewById<TextView>(R.id.res_name)
+        val resEmail = findViewById<TextView>(R.id.res_email)
+        val resGender = findViewById<TextView>(R.id.res_gender)
+        val resHighestQual = findViewById<TextView>(R.id.res_highest_qual)
+        val backBtn = findViewById<Button>(R.id.back_button)
 
-        val b:Bundle? = intent.extras;
+        val b:Bundle? = intent.extras
 
-        val name = b?.get("name").toString();
-        val email = b?.get("email").toString();
-        val gender = b?.get("gender").toString();
-        val highest_qual = b?.get("highest_qual").toString();
+        val name = b?.get("name").toString()
+        val email = b?.get("email").toString()
+        val gender = b?.get("gender").toString()
+        val highest_qual = b?.get("highest_qual").toString()
 
-        res_name.text = name;
-        res_email.text = email;
-        res_gender.text = gender
-        res_highest_qual.text = highest_qual;
+        resName.text = name
+        resEmail.text = email
+        resGender.text = gender
+        resHighestQual.text = highest_qual
+
+        backBtn.setOnClickListener{
+            finish()
+        }
     }
 }
